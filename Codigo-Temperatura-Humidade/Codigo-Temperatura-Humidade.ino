eb64283400c09e2aacf721eaeeec6512311cda6e
kukaslino <cauegirardi@gmail.com>
2023-12-04 18:57:30 -0300

    Subindo finalmente

--- Codigo-Temperatura-Humidade/Codigo-Temperatura-Humidade.ino
+++ Codigo-Temperatura-Humidade/Codigo-Temperatura-Humidade.ino
@@ -1,8 +1,9 @@
 #include "DHT.h"
-#define DHTPIN 2     // Pino digital sensor DHT
+#include "WiFi.h"
+#include <HTTPClient.h>
+#define DHTPIN 4
 
-// Escolha o tipo de sensor!
-# define DHTTYPE DHT11     // DHT 11
+# define DHTTYPE DHT11
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -10,12 +11,22 @@ DHT dht(DHTPIN, DHTTYPE);
 
 unsigned long previousMillis = 0;
 const long interval = 30000;
+const char* ssid = "Mi11";
+const char* password =  "Kukaslino1";
+String server = "https://pac6sensor.azurewebsites.net";
 
 void setup() {
   Serial.begin(9600);
 
   dht.begin();
 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.println("Connecting to WiFi..");
+  }
+ Serial.println("Connected to the WiFi network");
 }
 
 void loop() {
@@ -37,15 +48,36 @@ void loop() {
 
       previousMillis = currentMillis;
 
-      t = dht.readHumidity();
+      h = dht.readHumidity();
 
-      h = dht.readTemperature();
+      t = dht.readTemperature();
 
       Serial.print("Temperatura em Celsius: ");
-      Serial.println(h);
-      Serial.print("Humidade relativa do ar: ");
       Serial.println(t);
+      Serial.print("Humidade relativa do ar: ");
+      Serial.println(h);
 
+      HTTPClient http;
+
+      String serverPath = server + "/sensor?Humidity=" + h + "&Temperature=" + t + "&API_KEY=pac6";
+
+      http.begin(serverPath.c_str());
+
+      int httpResponseCode = http.GET();
+
+      if (httpResponseCode>0) {
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
+        String payload = http.getString();
+        Serial.println(payload);
+      }
+      else {
+        Serial.print("Error code: ");
+        Serial.println(httpResponseCode);
+      }
+      // Free resources
+      http.end();
+    
     }
 
   }
